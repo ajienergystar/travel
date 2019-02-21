206e5ae75c30f6afd4b90df7ee0fbd2887818825
Aji Prakosa <a.prakosa@teravintech.com>
2019-02-21 16:32:03 +0700

    Initial Commit

---
+++ Travel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Travel
+//
+//  Created by Aji Prakosa on 21/02/19.
+//  Copyright © 2019 Aji Prakosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
